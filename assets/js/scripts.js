5670fec2b7f2b2cdf6135d34cd61c342f18db969
“Sofia <sofimguina@gmail.com>
2024-06-30 22:40:19 -0400

    Version final

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+alert("¿Estás preparado para tu próxima aventura?");
+console.log("Bienvenido a CHILEVIAJA");
+
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
